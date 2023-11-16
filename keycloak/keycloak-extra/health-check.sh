bf85dd1442a2a513331b385ec0f901f2d2801b63
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2023-11-16 15:14:42 +0200

    Expose Keycloak via /idp

--- keycloak/keycloak-extra/health-check.sh
+++ keycloak/keycloak-extra/health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec 3<>/dev/tcp/localhost/8080
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:8080\n" >&3
+echo -e "GET ${KC_HTTP_RELATIVE_PATH}/health/ready HTTP/1.1\nhost: localhost:8080\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
