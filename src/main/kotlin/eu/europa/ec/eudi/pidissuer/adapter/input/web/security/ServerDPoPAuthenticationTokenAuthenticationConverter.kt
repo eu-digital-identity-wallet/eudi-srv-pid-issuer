d982e0ec9d6fd8f4eb73eb0835b8257d1b358a89
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2024-11-04 12:59:26 +0200

    Kotlinify DPoPTokenServerAccessDeniedHandler and ServerDPoPAuthenticationTokenAuthenticationConverter.

--- src/main/kotlin/eu/europa/ec/eudi/pidissuer/adapter/input/web/security/ServerDPoPAuthenticationTokenAuthenticationConverter.kt
+++ src/main/kotlin/eu/europa/ec/eudi/pidissuer/adapter/input/web/security/ServerDPoPAuthenticationTokenAuthenticationConverter.kt
@@ -18,6 +18,7 @@ package eu.europa.ec.eudi.pidissuer.adapter.input.web.security
 import com.nimbusds.jwt.SignedJWT
 import com.nimbusds.oauth2.sdk.token.AccessTokenType
 import com.nimbusds.oauth2.sdk.token.DPoPAccessToken
+import kotlinx.coroutines.reactor.mono
 import org.springframework.http.HttpHeaders
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.security.core.Authentication
@@ -33,74 +34,78 @@ import java.net.URI
  */
 class ServerDPoPAuthenticationTokenAuthenticationConverter : ServerAuthenticationConverter {
 
-    override fun convert(exchange: ServerWebExchange): Mono<Authentication> {
-        val request = exchange.request
-        val dpop = request.dPoP()
-        val authorization = request.authorization()
-        val method = request.method
-        val uri = request.uri()
+    override fun convert(exchange: ServerWebExchange): Mono<Authentication> =
+        mono {
+            try {
+                val request = exchange.request
+                val dpop = request.dPoP()
+                val authorization = request.authorization()
+                val uri = request.uri()
 
-        return Mono.zip(dpop, authorization, uri)
-            .onErrorMap {
-                when (it) {
-                    is OAuth2AuthenticationException -> it
-                    else -> {
-                        val error = DPoPTokenError.serverError("Unable to process DPoP request", it)
-                        OAuth2AuthenticationException(error, it)
-                    }
+                if (dpop != null && authorization != null) {
+                    DPoPTokenAuthentication.unauthenticated(dpop, authorization, request.method, uri)
+                } else {
+                    null
                 }
+            } catch (exception: OAuth2AuthenticationException) {
+                throw exception
+            } catch (exception: Exception) {
+                throw OAuth2AuthenticationException(
+                    DPoPTokenError.serverError(
+                        "Unable to process DPoP request",
+                        exception,
+                    ),
+                    exception,
+                )
             }
-            .map { DPoPTokenAuthentication.unauthenticated(it.t1, it.t2, method, it.t3) }
-    }
+        }
 }
 
 /**
  * Gets the value of a [header]. The header is expected to have at most 1 value. If more are found, an error is returned.
  */
-private fun HttpHeaders.singleValueHeader(header: String): Mono<String> {
+private fun HttpHeaders.singleValueHeader(header: String): String? {
     val values = this[header]
     return when {
-        values.isNullOrEmpty() -> Mono.empty()
-        values.size == 1 -> Mono.justOrEmpty(values[0])
-        else -> {
-            val error = DPoPTokenError.invalidRequest("Multiple '$values' header values found")
-            Mono.error(OAuth2AuthenticationException(error))
-        }
+        values.isNullOrEmpty() -> null
+        values.size == 1 -> values[0]
+        else -> throw OAuth2AuthenticationException(DPoPTokenError.invalidRequest("Multiple '$values' header values found"))
     }
 }
 
 /**
  * Gets the DPoP header value, if any, and parses it as a [SignedJWT].
  */
-private fun ServerHttpRequest.dPoP(): Mono<SignedJWT> = headers.singleValueHeader(AccessTokenType.DPOP.value)
-    .filter { !it.isNullOrBlank() }
-    .flatMap {
-        Mono.fromCallable { SignedJWT.parse(it) }
-            .onErrorMap {
-                val error =
-                    DPoPTokenError.invalidRequest("'${AccessTokenType.DPOP.value}' header is not a valid signed JWT")
-                OAuth2AuthenticationException(error)
+private fun ServerHttpRequest.dPoP(): SignedJWT? =
+    headers.singleValueHeader(AccessTokenType.DPOP.value)
+        ?.takeIf { it.isNotBlank() }
+        ?.let {
+            try {
+                SignedJWT.parse(it)
+            } catch (error: Exception) {
+                throw OAuth2AuthenticationException(
+                    DPoPTokenError.invalidRequest("'${AccessTokenType.DPOP.value}' header is not a valid signed JWT"),
+                )
             }
-    }
+        }
 
 /**
  * Gets the Authorization header value, if any.
  */
-private fun ServerHttpRequest.authorization(): Mono<DPoPAccessToken> = headers.singleValueHeader(HttpHeaders.AUTHORIZATION)
-    .filter { !it.isNullOrBlank() && it.startsWith(AccessTokenType.DPOP.value) }
-    .flatMap {
-        Mono.fromCallable { DPoPAccessToken.parse(it) }
-            .onErrorMap {
-                val error =
-                    DPoPTokenError.invalidRequest("'${HttpHeaders.AUTHORIZATION}' header is not a valid DPoP access token")
-                OAuth2AuthenticationException(error)
+private fun ServerHttpRequest.authorization(): DPoPAccessToken? =
+    headers.singleValueHeader(HttpHeaders.AUTHORIZATION)
+        ?.takeIf { it.isNotBlank() && it.startsWith(AccessTokenType.DPOP.value) }
+        ?.let {
+            try {
+                DPoPAccessToken.parse(it)
+            } catch (error: Exception) {
+                throw OAuth2AuthenticationException(
+                    DPoPTokenError.invalidRequest("'${HttpHeaders.AUTHORIZATION}' header is not a valid DPoP access token"),
+                )
             }
-    }
+        }
 
 /**
  * Gets the uri of the current [ServerHttpRequest]. The uri does not contain query parameters or fragments.
  */
-private fun ServerHttpRequest.uri(): Mono<URI> = Mono.fromCallable {
-    val uri = UriComponentsBuilder.fromUri(uri).query(null).fragment(null).toUriString()
-    URI.create(uri)
-}
+private fun ServerHttpRequest.uri(): URI = URI.create(UriComponentsBuilder.fromUri(uri).query(null).fragment(null).toUriString())
