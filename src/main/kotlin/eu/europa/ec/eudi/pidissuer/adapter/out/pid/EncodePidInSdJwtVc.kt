d1f1152e74275d74477c4e1d2d1fd03540654cf7
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2025-03-31 12:07:15 +0300

    Update email_address mapping.

--- src/main/kotlin/eu/europa/ec/eudi/pidissuer/adapter/out/pid/EncodePidInSdJwtVc.kt
+++ src/main/kotlin/eu/europa/ec/eudi/pidissuer/adapter/out/pid/EncodePidInSdJwtVc.kt
@@ -213,7 +213,7 @@ private fun selectivelyDisclosed(
         pid.familyNameBirth?.let { sdClaim(SdJwtVcPidClaims.BirthFamilyName.name, it.value) }
         pid.givenNameBirth?.let { sdClaim(SdJwtVcPidClaims.BirthGivenName.name, it.value) }
         pid.sex?.let { sdClaim(SdJwtVcPidClaims.Sex.name, it.value.toInt()) }
-        pid.emailAddress?.let { sdClaim(SdJwtVcPidClaims.EmailAddress.name, it) }
+        pid.emailAddress?.let { sdClaim(SdJwtVcPidClaims.Email.name, it) }
         pid.mobilePhoneNumber?.let { sdClaim(SdJwtVcPidClaims.MobilePhoneNumber.name, it.value) }
         sdObjClaim(SdJwtVcPidClaims.AgeEqualOrOver.attribute.name) {
             pid.ageOver18?.let { sdClaim(SdJwtVcPidClaims.AgeEqualOrOver.Over18.name, it) }
