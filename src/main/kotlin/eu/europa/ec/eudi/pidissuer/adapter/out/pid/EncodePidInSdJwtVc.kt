4e1c2df10724b2c25554694e771dbb3613eb245a
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2025-03-31 12:00:21 +0300

    Update mapping of birth_place.

--- src/main/kotlin/eu/europa/ec/eudi/pidissuer/adapter/out/pid/EncodePidInSdJwtVc.kt
+++ src/main/kotlin/eu/europa/ec/eudi/pidissuer/adapter/out/pid/EncodePidInSdJwtVc.kt
@@ -180,9 +180,14 @@ private fun selectivelyDisclosed(
         sdClaim(SdJwtVcPidClaims.FamilyName.name, pid.familyName.value)
         sdClaim(SdJwtVcPidClaims.GivenName.name, pid.givenName.value)
         sdClaim(SdJwtVcPidClaims.BirthDate.name, pid.birthDate.toString())
-        sdObjClaim(SdJwtVcPidClaims.PlaceOfBirth.attribute.name) {
-            sdClaim(SdJwtVcPidClaims.PlaceOfBirth.Locality.name, pid.birthPlace)
+        pid.birthPlace?.let { birthPlace ->
+            sdObjClaim(SdJwtVcPidClaims.PlaceOfBirth.attribute.name) {
+                birthPlace.country?.let { sdClaim(SdJwtVcPidClaims.PlaceOfBirth.Country.name, it.value) }
+                birthPlace.region?.let { sdClaim(SdJwtVcPidClaims.PlaceOfBirth.Region.name, it.value) }
+                birthPlace.locality?.let { sdClaim(SdJwtVcPidClaims.PlaceOfBirth.Locality.name, it.value) }
+            }
         }
+
         sdArrClaim(SdJwtVcPidClaims.Nationalities.name) {
             pid.nationalities.forEach { sdClaim(it.value) }
         }
