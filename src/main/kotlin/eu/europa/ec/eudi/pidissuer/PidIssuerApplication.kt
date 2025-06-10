19e2311582631906bb8608803643ef7b67c34f74
Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>
2025-06-10 10:58:56 +0300

    Minor changes
    Signed-off-by: Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>

--- src/main/kotlin/eu/europa/ec/eudi/pidissuer/PidIssuerApplication.kt
+++ src/main/kotlin/eu/europa/ec/eudi/pidissuer/PidIssuerApplication.kt
@@ -163,7 +163,7 @@ internal object WebClients {
             .build()
     }
 
-    private fun httpClient(proxy: HttpProxy?): HttpClient {
+    private fun httpClient(proxy: HttpProxy? = null): HttpClient {
         if (proxy == null) {
             return HttpClient.create()
         }
@@ -198,6 +198,8 @@ data class HttpProxy(
  * [Client] instances for usage within the application.
  */
 internal object RestEasyClients {
+    private const val PROXY_HOST_PROP = "org.jboss.resteasy.jaxrs.client.proxy.host"
+    private const val PROXY_PORT_PROP = "org.jboss.resteasy.jaxrs.client.proxy.port"
 
     /**
      * A [Client].
@@ -209,8 +211,8 @@ internal object RestEasyClients {
             false,
         )
         return proxy?.let {
-            client.property("org.jboss.resteasy.jaxrs.client.proxy.host", proxy.url)
-                .property("org.jboss.resteasy.jaxrs.client.proxy.port", proxy.url.port)
+            client.property(PROXY_HOST_PROP, proxy.url)
+                .property(PROXY_PORT_PROP, proxy.url.port)
         } ?: client
     }
 
