70b0be25223c202f07c5d4f327f294338c790711
Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>
2026-05-28 13:49:14 +0300

    Update Keycloak health check, image, and dependencies
    - Upgraded Keycloak base image to `26.6.2-2`
    - Updated health check script
    - Enabled Keycloak health-related configurations
    - Bumped `nimbusJoseJwt` and `nimbusOAuth2` dependencies
    
    Signed-off-by: Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>

--- docker-compose/keycloak/extra/health-check.sh
+++ docker-compose/keycloak/extra/health-check.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-exec 3<>/dev/tcp/localhost/8080
 
-echo -e "GET ${KC_HTTP_RELATIVE_PATH} HTTP/1.1\nHost: localhost\n" >&3
-timeout --preserve-status 1 cat <&3 | grep -m 1 "HTTP/1.1" | grep -m 1 "303 See Other"
+PORT=9000
+HEALTH_PATH="/idp/health/ready"
+
+timeout 3 bash -c '
+  exec 3<>/dev/tcp/localhost/'"$PORT"' || exit 1
+  echo -e "GET '"$HEALTH_PATH"' HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n" >&3 || exit 1
+  grep -m 1 "HTTP/1.1 200 OK" <&3 > /dev/null
+'
 ERROR=$?
 
-exec 3<&-
-exec 3>&-
-
-exit $ERROR
+if [ "$ERROR" -eq 0 ]; then
+  echo "Healthcheck Successful"
+  exit 0
+else
+  echo "Healthcheck Failed"
+  exit 1
+fi
